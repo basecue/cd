deb9188719199005a62f37da7c47aadf996c0fe1
Jan Češpivo <jan.cespivo@gmail.com>
2016-02-29 21:23:05 +0100

    output reader fix

--- codev/providers/performers/local.py
+++ codev/providers/performers/local.py
@@ -16,8 +16,6 @@
 
 from subprocess import Popen, PIPE, call
 
-from threading import Thread
-
 from codev.performer import CommandError, BasePerformer, Performer, OutputReader
 
 from logging import getLogger
@@ -26,24 +24,10 @@ from logging import getLogger
 class LocalPerformer(BasePerformer):
     def __init__(self, *args, **kwargs):
         super(LocalPerformer, self).__init__(*args, **kwargs)
-        self._output_lines = []
-        self._error_lines = []
         self.logger = getLogger(__name__)
-
-    def _reader_out(self, stream, logger=None):
-        while True:
-            line = stream.readline()
-            if not line:
-                break
-            output_line = line.decode('utf-8').strip()
-            self._output_lines.append(output_line)
-            (logger or self.output_logger).debug(output_line)
-        stream.close()
 
     def execute(self, command, logger=None, writein=None):
         self.logger.debug('Executing LOCAL command: %s' % command)
-        self._output_lines = []
-        self._error_lines = []
         process = Popen(command, stdout=PIPE, stderr=PIPE, stdin=PIPE, shell=True)
 
         # read stdout asynchronously - in 'realtime'
@@ -55,17 +39,16 @@ class LocalPerformer(BasePerformer):
             process.stdin.close()
 
         # wait for end of output
-        output_reader.output()
+        output = output_reader.output()
 
         # wait for exit code
         exit_code = process.wait()
-
 
         if exit_code:
             # read stderr for error output
             err = process.stderr.read().decode('utf-8').strip()
             raise CommandError(command, exit_code, err)
-        return '\n'.join(self._output_lines)
+        return output
 
     def send_file(self, source, target):
         call('cp', source, target)
