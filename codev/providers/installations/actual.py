6f81cd404855640007cf918747f5d6ac98a44c98
Jan Češpivo <jan.cespivo@gmail.com>
2016-04-09 12:51:30 +0200

    fix actual installattion

--- codev/providers/installations/actual.py
+++ codev/providers/installations/actual.py
@@ -1,16 +1,20 @@
 from codev.installation import Installation, BaseInstallation
 import shutil
 from time import time
+from uuid import uuid1
 
 
 class ActualInstallation(BaseInstallation):
     def install(self, performer):
-        # gunzip is in default ubuntu
-        # performer.execute('apt-get install gunzip -y --force-yes')
-        archive = shutil.make_archive('/tmp/', 'gztar')
+        archive = shutil.make_archive('/tmp/{filename}'.format(filename=uuid1()), 'gztar')
         performer.send_file(archive, archive)
+        performer.install_packages('gunzip')
         performer.execute('mkdir -p {directory}'.format(directory=self.directory))
-        performer.execute('tar -xzf {archive} --directory {directory}'.format(archive=archive, directory=self.directory))
+        performer.execute(
+            'tar -xzf {archive} --directory {directory}'.format(
+                archive=archive, directory=self.directory
+            )
+        )
 
     def process_options(self, options):
         self.uid = options or str(time())
