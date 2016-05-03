02657576eed9ced59352ed42f3b0f7b3b87e3cb1
Jan Češpivo <jan.cespivo@gmail.com>
2016-05-03 16:59:34 +0200

    fixies

--- codev/performer.py
+++ codev/performer.py
@@ -13,7 +13,7 @@ COMMON_SCRIPTS_PATH = '{directory}/scripts'.format(directory=path.dirname(__file
 class BaseExecutor(object):
     def __init__(self, *args, ident=None, **kwargs):
         self.base_dir = ''
-        self.working_dir = self.base_dir
+        self.working_dirs = []
         self.ident = ident or str(time())
         self.output_logger = getLogger('command_output')
         super().__init__(*args, **kwargs)
@@ -24,6 +24,12 @@ class BaseExecutor(object):
             return True
         except CommandError:
             return False
+
+    def _prepare_command(self, command):
+        return 'bash -c "cd {working_dir} && {command}"'.format(
+            working_dir=self.working_dir,
+            command=command.replace('\\', '\\\\').replace('$', '\$').replace('"', '\\"')
+        )
 
     def execute(self, command, logger=None, writein=None, max_lines=None):
         raise NotImplementedError()
@@ -55,11 +61,14 @@ class BaseExecutor(object):
             arguments.update(common_arguments)
             self.run_script(script, arguments, logger=logger)
 
+    def working_dir(self):
+        path.join(self.base_dir, self.working_dirs)
+
     @contextmanager
     def change_directory(self, directory):
-        self.working_dir = path.join(self.base_dir, directory)
+        self.working_dirs.append(directory)
         yield
-        self.working_dir = self.base_dir
+        self.working_dirs.pop()
 
 
 class PerformerError(Exception):
@@ -110,32 +119,61 @@ class OutputReader(object):
         return '\n'.join(self._output_lines)
 
 
+DISTRIBUTION_ISSUES = {
+    'debian': 'Debian',
+    'ubuntu': 'Ubuntu',
+    'arch': 'Arch Linux',
+}
+
+
 class BasePerformer(BaseExecutor):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__cache_packages = False
+
+    def _distribution(self):
+        if not self.__distribution:
+            issue = self.execute('cat /etc/issue')
+            for distribution, issue_start in DISTRIBUTION_ISSUES.items():
+                if issue.startswith(issue_start):
+                    self.__distribution = distribution
+                    break
+            else:
+                raise PerformerError('Unknown distribution')
 
     def install_packages(self, *packages):
         # TODO make this os independent
         not_installed_packages = [package for package in packages if not self._is_package_installed(package)]
         if not_installed_packages:
             self._cache_packages()
-            self.execute(
-                'DEBIAN_FRONTEND=noninteractive apt-get install {packages} -y --force-yes'.format(
-                    packages=' '.join(not_installed_packages)
+            if self._distribution() in ('debian', 'ubuntu'):
+                self.execute(
+                    'DEBIAN_FRONTEND=noninteractive apt-get install {packages} -y --force-yes'.format(
+                        packages=' '.join(not_installed_packages)
+                    )
                 )
-            )
+            elif self._distribution() == 'arch':
+                self.execute(
+                    'pacman -S {packages} --no-confirm'.format(
+                        packages=' '.join(not_installed_packages)
+                    )
+                )
 
     def _cache_packages(self):
         if not self.__cache_packages:
-            self.execute('apt-get update')
+            if self._distribution() in ('debian', 'ubuntu'):
+                self.execute('apt-get update')
         self.__cache_packages = True
 
     def _is_package_installed(self, package):
         # http://www.cyberciti.biz/faq/find-out-if-package-is-installed-in-linux/
         # TODO make this os independent
         try:
-            return 'install ok installed' == self.execute("dpkg-query -W -f='${{Status}}' {package}".format(package=package))
+            if self._distribution() in ('debian', 'ubuntu'):
+                return 'install ok installed' == self.execute(
+                    "dpkg-query -W -f='${{Status}}' {package}".format(package=package))
+            elif self._distribution() == 'arch':
+                return self.check_execute("pacman -Qi {package}".format(package=package))
         except CommandError:
             return False
 
--- codev/providers/isolators/directory.py
+++ codev/providers/isolators/directory.py
@@ -6,25 +6,25 @@ class DirectoryIsolator(Isolator):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._dir = '~/.share/codev/{ident}/directory'.format(ident=self.ident)
+        self.base_dir = '~/.share/codev/{ident}/directory'.format(ident=self.ident)
 
     def exists(self):
-        return self.performer.check_execute('[ -d {dir} ]'.format(dir=self._dir))
+        return self.performer.check_execute('[ -d {dir} ]'.format(dir=self.base_dir))
 
     def create(self):
-        self.performer.execute('mkdir -p {dir}'.format(dir=self._dir))
+        self.performer.execute('mkdir -p {dir}'.format(dir=self.base_dir))
 
     def is_started(self):
         return self.exists
 
     def destroy(self):
-        return self.performer.execute('rm -rf {dir}'.format(dir=self._dir))
+        return self.performer.execute('rm -rf {dir}'.format(dir=self.base_dir))
 
     def execute(self, command, logger=None, writein=None, max_lines=None):
-        with self.performer.change_directory(self._dir):
+        with self.performer.change_directory(self.base_dir):
             return super().execute(
-                'bash -c "cd {working_dir} && {command}"'.format(
-                    working_dir=self.performer.working_dir,
+                'bash -c "cd {base_dir} && {command}"'.format(
+                    base_dir=self.base_dir,
                     command=command.replace('\\', '\\\\').replace('$', '\$').replace('"', '\\"')
-                ), logger=None, writein=None, max_lines=None
+                ), logger=logger, writein=writein, max_lines=max_lines
             )
