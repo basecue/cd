eed3218f4a22268c469045eb1e183379e0fe507d
Jan Češpivo <jan.cespivo@gmail.com>
2016-05-15 15:20:11 +0200

    implementation of real machine

--- codev/providers/machines/real.py
+++ codev/providers/machines/real.py
@@ -3,9 +3,29 @@ from codev.machines import MachinesProvider, BaseMachine
 
 
 class RealMachine(BaseMachine):
-    def __init__(self, ip):
-        self.ip = ip
-        #TODO
+    def exists(self):
+        return True
+
+    def is_started(self):
+        return True
+
+    def start(self):
+        pass
+
+    def create(self, distribution, release, install_ssh_server=False, ssh_key=None):
+        pass
+
+    def destroy(self):
+        pass
+
+    def stop(self):
+        pass
+
+    @property
+    def ip(self):
+        # TODO rename to host
+        # TODO this is workaround
+        return self.ident
 
 
 class RealMachinesSettings(BaseSettings):
@@ -17,4 +37,10 @@ class RealMachinesSettings(BaseSettings):
 class RealMachinesProvider(MachinesProvider):
     provider_name = 'real'
     settings_class = RealMachinesSettings
+    machine_class = RealMachine
+
+    def idents(self):
+        # TODO this is workaround
+        for host in self.settings.hosts:
+            yield host
 
