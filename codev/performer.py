6593a504eb1ca3120a236410523ddd7074ba4e79
Jan Češpivo <jan.cespivo@gmail.com>
2016-09-09 11:03:23 +0200

    custom scripts have higher priority then common codev scripts

--- codev/performer.py
+++ codev/performer.py
@@ -5,7 +5,6 @@ from json import dumps
 from codev.scripts import COMMON_SCRIPTS
 
 
-# TODO - codev/ -> common/ ?
 COMMON_SCRIPTS_PREFIX = 'codev/'
 COMMON_SCRIPTS_PATH = '{directory}/scripts'.format(directory=path.dirname(__file__))
 
@@ -51,18 +50,20 @@ class BaseExecutor(object):
             arguments = {}
 
         # common scripts
-        for common_script, script_path in COMMON_SCRIPTS.items():
-            script_ident = '{prefix}{common_script}'.format(
-                prefix=COMMON_SCRIPTS_PREFIX,
-                common_script=common_script
-            )
-            if script.startswith(script_ident):
-                script_replace = '{common_scripts_path}/{script_path}'.format(
-                    common_scripts_path=COMMON_SCRIPTS_PATH,
-                    script_path=script_path
+        if not path.isfile(script): # custom scripts have higher priority
+            for common_script, script_path in COMMON_SCRIPTS.items():
+                script_ident = '{prefix}{common_script}'.format(
+                    prefix=COMMON_SCRIPTS_PREFIX,
+                    common_script=common_script
                 )
-                script = script.replace(script_ident, script_replace, 1)
-                break
+
+                if script.startswith(script_ident):
+                    script_replace = '{common_scripts_path}/{script_path}'.format(
+                        common_scripts_path=COMMON_SCRIPTS_PATH,
+                        script_path=script_path
+                    )
+                    script = script.replace(script_ident, script_replace, 1)
+                    break
 
         return self.execute(script.format(**arguments), writein=dumps(arguments), logger=logger)
 
